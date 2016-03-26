f0c834beecb5187435e5414a59764b645f518c7f
Wolfgang A Criollo <wolfplisken@gmail.com>
2016-03-26 18:59:22 -0400

    set upt angular

--- js/app/mainApp.js
+++ js/app/mainApp.js
@@ -1,12 +1,6 @@
 (function(){
-    var myApp = angular.module('mainApp',["ngRoute","events"]);
-    config(["$routeProvider"],$locationPorvider,function($routeProvider,$locationProvider){
-          $routeProvider.when('/page1',{templateUrl:'partials/navigatingv5'});
-          $routeProvider.when('/page2',{templateUrl:'partials/responsive'});
-          $routeProvider.when('/page3',{templateUrl:'partials/structure'});
-          $routeProvider.otherwise({redirect: '/home'});
-        })
-    
-            $locationProvider.html5Mode({enable:true,requireBase:false});
-})()//end function
-
+    var myApp = angular.module('mainApp',['ngRoute']);
+  myApp.controller("nav", function($scope){
+    $scope.message = "I'm Working ";
+  });
+})();//end function
