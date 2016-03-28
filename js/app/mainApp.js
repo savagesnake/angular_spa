632a826d118deb0c5ad4543a5bdeb066a4de29f4
Wolfgang A Criollo <wolfplisken@gmail.com>
2016-03-27 22:50:28 -0400

    set up the main navigation with angular ui-route

--- js/app/mainApp.js
+++ js/app/mainApp.js
@@ -1,6 +1,87 @@
 (function(){
-    var myApp = angular.module('mainApp',['ngRoute']);
-  myApp.controller("nav", function($scope){
-    $scope.message = "I'm Working ";
-  });
+  // CREATE A MODULE AND NAME IT
+  // Also include the ngRoute for all our routing
+var myApp = angular.module('mainApp',['ui.router']);
+
+
+myApp.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider){
+
+$urlRouterProvider.otherwise('/home');
+
+$stateProvider
+.state('home',{
+  url:'/home',
+  templateUrl:'/pages/home.html',
+  controller:'homeCrt'
+})
+.state('structure',{
+  url:'/structure',
+  templateUrl:'pages/structure.html',
+  controller:'structureCrt'
+}).state('navigatingv5', {
+  url: "/navigatingv5",
+  templateUrl: "pages/navigatingv5.html"
+})
+.state('createNew', {
+  url: "/createNew",
+  templateUrl: "pages/createNew.html",
+  controller:'createnewCrt'
+})
+.state('responsive', {
+  url: "/responsive",
+  templateUrl: "pages/responsive.html"
+})
+.state('javascript', {
+  url: "/javascript",
+  templateUrl: "pages/javascript.html"
+})
+
+
+  //  route for the home page
+  //  $routeProvider
+  // .when('/',{
+  //   templateUrl:'/pages/home.html',
+  //   controller:'homeCrt'
+  // })
+  // .when('/createNew',{
+  //   templateUrl:'/pages/createNew.html',
+  //   controller:'createnewCrt'
+  // })
+  // .when('/navigatingv5',{
+  //   templateUrl:'/pages/navigatingv5.html',
+  //   controller:'navigateCrt'
+  // })
+  // .when('/responsive',{
+  //   templateUrl:'/pages/responsive.html',
+  //   controller:'resposiveCrt'
+  // })
+  // .when('/structure',{
+  //   templateUrl:'/pages/structure.html',
+  //   controller:'structureCrt'
+  // })
+  // .otherwise({
+  //   redirecTo:'/'
+  // });
+
+// use the HTML5 history APi
+// $locationProvider.html5Mode(true);
+  // end config
+}]);
+
+ myApp.controller('homeCrt', function($scope){
+   $scope.heading = "V5 Guide";
+ });
+ myApp.controller('createnewCrt', function($scope){
+   $scope.heading = "Create a new website";
+ });
+ myApp.controller('navigateCrt', function($scope){
+   $scope.heading = "Navigate V5";
+ });
+ myApp.controller('resposiveCrt', function($scope){
+   $scope.heading = "Setting up responsive";
+ });
+ myApp.controller('structureCrt', function($scope){
+   $scope.heading = "V5 Main html structure";
+ });
+
 })();//end function
