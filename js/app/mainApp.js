f0348c6ac41ec974651ef75de35115c9edfdf26c
Wolfgang A Criollo <wolfplisken@gmail.com>
2016-04-03 03:27:55 -0400

    style the section head form the main content add glyphicons, update all of the pages with the new main body section, and updated the controller on the myApp js file to use scope and interpolations

--- js/app/mainApp.js
+++ js/app/mainApp.js
@@ -12,46 +12,58 @@ $stateProvider
 .state('home',{
   url:'/home',
   templateUrl:'/pages/home.html',
-  controller:'homeCrt'
+  controller:'homeCtr'
 })
 .state('structure',{
   url:'/structure',
   templateUrl:'pages/structure.html',
-  controller:'structureCrt'
+  controller:'structureCtr'
 }).state('navigatingv5', {
   url: "/navigatingv5",
-  templateUrl: "pages/navigatingv5.html"
+  templateUrl: "pages/navigatingv5.html",
+  controller:'navigateCtr'
 })
 .state('createNew', {
   url: "/createNew",
   templateUrl: "pages/createNew.html",
-  controller:'createnewCrt'
+  controller:'createnewCtr'
 })
 .state('responsive', {
   url: "/responsive",
-  templateUrl: "pages/responsive.html"
+  templateUrl: "pages/responsive.html",
+  controller:'resposiveCtr'
 })
 .state('javascript', {
   url: "/javascript",
-  templateUrl: "pages/javascript.html"
+  templateUrl: "pages/javascript.html",
+  controller:'javascriptCtr'
 });
   // end config
 }]);
 
- myApp.controller('homeCrt', function($scope){
+ myApp.controller('homeCtr', function($scope){
    $scope.heading = "V5 Guide";
+   $scope.pageHeading = 'Welcome to v5 guide';
  });
- myApp.controller('createnewCrt', function($scope){
+ myApp.controller('createnewCtr', function($scope){
    $scope.heading = "Create a new website";
+    $scope.pageHeading = 'How To Create a New Website';
  });
- myApp.controller('navigateCrt', function($scope){
+ myApp.controller('navigateCtr', function($scope){
    $scope.heading = "Navigate V5";
+   $scope.pageHeading = 'How To Move Around V5';
  });
- myApp.controller('resposiveCrt', function($scope){
+ myApp.controller('resposiveCtr', function($scope){
    $scope.heading = "Setting up responsive";
+   $scope.pageHeading = 'Setting Up Responsive';
  });
- myApp.controller('structureCrt', function($scope){
+ myApp.controller('structureCtr', function($scope){
    $scope.heading = "V5 Main html structure";
+   $scope.pageHeading = 'V5 HTML Node Structure';
+ });
+ myApp.controller('javascriptCtr',function($scope){
+   $scope.heading = 'Javascript Functions';
+   $scope.pageHeading = 'Custom Javascript Medoths';
  });
 
 })();//end function
